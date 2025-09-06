3400d0704ca9044c6aa3cb1696ded103e5edbfc8
theoutom <125547899+theoutom@users.noreply.github.com>
2025-09-06 23:25:15 +0700

    Mengubah value, menghapus key dan value, serta mengubah key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 lst_nama_siswa = ['Satrio: 90', 'Tio: 79', 'Sat: 96', 'Utomo: 89', 'Tomo: 83', 'Ernesto: 98', 'Nesto: 81'] 
 
-dic_nilai_siswa = { 'Satrio': 90,
+dict_nilai_siswa = { 'Satrio': 90,
                'Tio': 79,
                'Sat': 96,
                'Utomo': 89,
@@ -15,3 +15,12 @@ print(lst_nama_siswa)
 print(f"Siswa pertama adalah {lst_nama_siswa[0]}")
 print(f"Siswa terakhir adalah {lst_nama_siswa[-1]}")
 print(f"Tiga siswa terakhir adalah {lst_nama_siswa[-4:-1]}")
+
+dict_nilai_siswa['Satrio'] = 99
+print(dict_nilai_siswa)
+
+dict_nilai_siswa.pop('Tio')
+print(dict_nilai_siswa)
+
+dict_nilai_siswa['Tomo'] = dict_nilai_siswa['Hamup']
+print(dict_nilai_siswa)
