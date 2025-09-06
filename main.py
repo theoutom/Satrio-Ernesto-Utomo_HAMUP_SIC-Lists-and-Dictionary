e254db744ccdaa3e85724be49c8921f38090af6e
theoutom <125547899+theoutom@users.noreply.github.com>
2025-09-06 22:32:16 +0700

    menambahkan list untuk update data ke dictionary nilai_siswa

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-nilai_siswa = { "Satrio": 90,
-               "Tio": 79,
-               "Sat": 96,
-               "Utomo": 89,
-               "Tomo": 83,
-               "Ernesto": 98,
-               "Nesto": 81}
+nilai_siswa = { 'Satrio': 90,
+               'Tio': 79,
+               'Sat': 96,
+               'Utomo': 89,
+               'Tomo': 83,
+               'Ernesto': 98,
+               'Nesto': 81}
+
+new_data = [('Toto', 75)]
