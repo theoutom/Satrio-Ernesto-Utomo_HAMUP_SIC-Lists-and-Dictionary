3dcae265ced7a10ca0c06c12f53be129843b6521
theoutom <125547899+theoutom@users.noreply.github.com>
2025-09-06 23:11:58 +0700

    menambahkan print slicing untuk list dengan ketentuan masing-masing

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ dic_nilai_siswa = { 'Satrio': 90,
 lst_nama_siswa.append('Momo: 75')
 lst_nama_siswa.remove('Tio: 79')
 print(lst_nama_siswa)
+
+print(f"Siswa pertama adalah {lst_nama_siswa[0]}")
+print(f"Siswa terakhir adalah {lst_nama_siswa[-1]}")
+print(f"Tiga siswa terakhir adalah {lst_nama_siswa[-3:-1]}")
